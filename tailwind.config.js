ad0995b37006cc1eb83824f0124cb1030a0de9ec
Piyush0692 <155516296+Piyush0692@users.noreply.github.com>
2025-08-30 12:18:17 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+<<<<<<< HEAD
+    extend: {
+      fontFamily: {
+        montreal: ["Montreal", "sans-serif"],
+        FoundersGroteskXCondensed: ["Founders Grotesk X Condensed", "sans-serif"],
+      }
+    },
+=======
+    extend: {},
+>>>>>>> 9ce856d1847c15f9d42e5c668a7298c3fe901315
+  },
+  plugins: [],
+}
+
